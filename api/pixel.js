8915956feecb13cf15af6f8834a42e20e2e63f96
ikho131 <ikho.3line@gmail.com>
2025-04-04 12:20:59 +0900

    Update pixel.js

--- api/pixel.js
+++ api/pixel.js
@@ -1,1 +1,13 @@
+export default function handler(req, res) {
+  console.log("📩 이메일 트래킹 픽셀 호출됨");
 
+  const pixel = Buffer.from(
+    "R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==",
+    "base64"
+  );
+  res.setHeader("Content-Type", "image/gif");
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  res.status(200).send(pixel);
+}
